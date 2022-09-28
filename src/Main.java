4c5f426577a0159f8a15c80e0b7e824365e24ea9
bilal karahan <bilalkarahan309@gmail.com>
2022-09-28 13:48:00 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
+    static double us(double a, double b){
+        if (b==1){
+            return a;
+        } else {
+            return us(a,b-1)*a;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in=new Scanner(System.in);
+        System.out.println("a sayısını giriniz: ");
+        double a= in.nextDouble();
+        System.out.println("b sayısını giriniz: ");
+        double b= in.nextDouble();
+        System.out.println(us(a,b));
     }
 }
